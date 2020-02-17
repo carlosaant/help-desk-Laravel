98e0aae8b4bcdddce9c6fb779c282d7cf50cfc32
Antonio Carlos <carlos_antonio.minga@outlook.com>
2020-02-17 15:53:16 -0300

    Create table solicitante utilizando o php artisan make:

---
+++ database/migrations/2020_02_17_184120_create_solicitante_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolicitanteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicitante', function (Blueprint $table) {
+           // $table->bigIncrements('id');
+            $table->string('email', '70')->unique();
+            $table->string('nome', '70');
+            $table->string('telefone', '20')->nullable();
+            $table->string('observacao', '255')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicitante');
+    }
+}
